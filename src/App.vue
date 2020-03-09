adb114aa585e67c3b0b740aa0927cdddf4a4566a
issei morita <isseimorita1@gmail.com>
2020-03-08 23:52:25 -0700

    made clickable world map component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<div id="contents">
+  <GmapMap id="gmap" ref="gmap" :center="{lat: 0, lng: 0}" :zoom="1.6" map-type-id="hybrid" :options="gmapOptions" @click="clickCountry"></GmapMap>
+</div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+<script>
+import { Component, Vue } from 'vue-property-decorator'
+import { gmapApi } from 'vue2-google-maps'
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+@Component({})
+export default class App extends Vue {
+  gmapOptions = {
+    mapTypeControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: false,
+    disableDefaultUi: false,
+    draggable: true,
+    minZoom: 1.6,
+    maxZoom: 4
+  }
+  get google() {
+    return gmapApi
+  }
+  mounted() {
+    this.$refs.gmap.$mapPromise.then((map) => {})
+  }
+
+  clickCountry(e) {
+    const latLng = new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
+    new google.maps.Geocoder().geocode({ latLng }, (results, status) => {
+      if (status === google.maps.GeocoderStatus.OK) {
+        if (results[1]) {
+          let country = null
+          let countryCode = null
+
+          for (let r = 0, rl = results.length; r < rl; r += 1) {
+            const result = results[r]
+
+            if (!country && result.types[0] === 'country') {
+              country = result.address_components[0].long_name
+              countryCode = result.address_components[0].short_name
+            }
+          }
+
+          console.log('Country: ' + country + ', Country Code: ' + countryCode)
+        }
+      }
+    })
+  }
+}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+#gmap {
+  width: 768px;
+  min-height: 768px;
+  padding: 0;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+// @ts-ignore
+import * as VueGoogleMaps from 'vue2-google-maps'
 
-Vue.config.productionTip = false;
+import './plugins/vuetify'
+
+Vue.config.productionTip = false
+
+
+require('dotenv').config()
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_API_KEY
+  }
+})
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  vuetify,
+  render: (h) => h(App)
 }).$mount('#app');
